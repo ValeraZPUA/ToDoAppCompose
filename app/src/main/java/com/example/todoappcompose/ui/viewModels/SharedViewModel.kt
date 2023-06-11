3fe341413fbb7fdc9974e88d1843d3934bd99274
Valery Lyuty <lvv0490@gmail.com>
2023-06-11 11:09:57 +0300

    new task adding

--- app/src/main/java/com/example/todoappcompose/ui/viewModels/SharedViewModel.kt
+++ app/src/main/java/com/example/todoappcompose/ui/viewModels/SharedViewModel.kt
@@ -7,10 +7,12 @@ import androidx.lifecycle.viewModelScope
 import com.example.todoappcompose.data.models.Priority
 import com.example.todoappcompose.data.models.ToDoTask
 import com.example.todoappcompose.data.repositories.ToDoRepository
+import com.example.todoappcompose.util.Action
 import com.example.todoappcompose.util.Constants.MAX_TITLE_LENGTH
 import com.example.todoappcompose.util.RequestState
 import com.example.todoappcompose.util.SearchAppBarState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collect
@@ -21,6 +23,8 @@ import javax.inject.Inject
 class SharedViewModel @Inject constructor(
     private val toDoRepository: ToDoRepository
 ) : ViewModel() {
+
+    val action: MutableState<Action> = mutableStateOf(Action.NO_ACTION)
 
     val id: MutableState<Int> = mutableStateOf(0)
     val title: MutableState<String> = mutableStateOf("")
@@ -36,6 +40,41 @@ class SharedViewModel @Inject constructor(
 
     private val _selectedTask = MutableStateFlow<ToDoTask?>(null)
     val selectedTask: StateFlow<ToDoTask?> = _selectedTask
+
+    private fun addTask() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val toDoTask = ToDoTask(
+                title = title.value,
+                description = description.value,
+                priority = priority.value
+            )
+            toDoRepository.addTask(toDoTask = toDoTask)
+        }
+    }
+
+    fun handleDatabaseActions(action: Action) {
+        when (action) {
+            Action.ADD -> {
+                addTask()
+            }
+            Action.UPDATE -> {
+
+            }
+            Action.DELETE -> {
+
+            }
+            Action.DELETE_ALL -> {
+
+            }
+            Action.UNDO -> {
+
+            }
+            else -> {
+
+            }
+        }
+        this.action.value = Action.NO_ACTION
+    }
 
     fun getAllTasks() {
         _allTAsks.value = RequestState.Loading
