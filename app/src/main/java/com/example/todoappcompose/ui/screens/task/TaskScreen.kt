4897ee7879ea82a407e818fadc0518274b047c6a
Lyuty Valery <lvv0490@gmail.com>
2023-08-01 12:29:48 +0300

    Intercept Back Button

--- app/src/main/java/com/example/todoappcompose/ui/screens/task/TaskScreen.kt
+++ app/src/main/java/com/example/todoappcompose/ui/screens/task/TaskScreen.kt
@@ -3,9 +3,15 @@ package com.example.todoappcompose.ui.screens.task
 import android.annotation.SuppressLint
 import android.content.Context
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.OnBackPressedDispatcher
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.platform.LocalContext
 import com.example.todoappcompose.R
 import com.example.todoappcompose.data.models.Priority
@@ -26,6 +32,12 @@ fun TaskScreen(
     val priority: Priority by sharedViewModel.priority
 
     val context = LocalContext.current
+
+    BackHandler(
+        onBackPress = {
+            navigateToListScreen(Action.NO_ACTION)
+        }
+    )
 
     Scaffold(
         topBar = {
@@ -65,4 +77,29 @@ fun TaskScreen(
 
 private fun displayToast(context: Context) {
     Toast.makeText(context, context.getString(R.string.fields_empty), Toast.LENGTH_SHORT).show()
+}
+
+@Composable
+fun BackHandler(
+    backDispatcher: OnBackPressedDispatcher? = LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher,
+    onBackPress: () -> Unit
+) {
+    val currentOnBackPressed by rememberUpdatedState(newValue = onBackPress)
+
+    val backCallback = remember {
+        object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                currentOnBackPressed()
+            }
+
+        }
+    }
+    DisposableEffect(key1 = backDispatcher) {
+        backDispatcher?.addCallback(backCallback)
+
+        onDispose {
+            backCallback.remove()
+        }
+    }
+
 }
