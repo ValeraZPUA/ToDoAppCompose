c5aa5c42a14e449e1bd6bb0f6e14640766eb8873
Lyuty Valery <lvv0490@gmail.com>
2023-08-01 10:30:53 +0300

    Animate Item Visibility

--- app/src/main/java/com/example/todoappcompose/ui/screens/list/ListContent.kt
+++ app/src/main/java/com/example/todoappcompose/ui/screens/list/ListContent.kt
@@ -1,6 +1,10 @@
 package com.example.todoappcompose.ui.screens.list
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -25,7 +29,12 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.rememberDismissState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
@@ -47,6 +56,8 @@ import com.example.todoappcompose.ui.theme.taskItemTextColor
 import com.example.todoappcompose.util.Action
 import com.example.todoappcompose.util.RequestState
 import com.example.todoappcompose.util.SearchAppBarState
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun ListContent(
@@ -145,7 +156,13 @@ fun DisplayTasks(
             val dismissDirection = dismissState.dismissDirection
             val isDismissed = dismissState.isDismissed(DismissDirection.EndToStart)
             if (isDismissed && dismissDirection == DismissDirection.EndToStart) {
-                onSwipeToDelete(Action.DELETE, task)
+                val scope = rememberCoroutineScope()
+                LaunchedEffect(key1 = null) {
+                    scope.launch {
+                        delay(300)
+                        onSwipeToDelete(Action.DELETE, task)
+                    }
+                }
             }
             val degrees by animateFloatAsState(
                 if (dismissState.targetValue == DismissValue.Default) {
@@ -155,23 +172,42 @@ fun DisplayTasks(
                 }
             )
 
-            SwipeToDismiss(
-                state = dismissState,
-                directions = setOf(DismissDirection.EndToStart),
-                dismissThresholds = {
-                    FractionalThreshold(fraction = 0.2f)
-                },
-                background = {
-                    RedBackground(degrees = degrees)
-                },
-                dismissContent = {
-                    TaskItem(
-                        toDoTask = task,
-                        navigateToTaskScreen = { navigateToTaskScreen(task.id) }
-                    )
-                }
+            var itemAppeared by remember { mutableStateOf(false) }
+            LaunchedEffect(key1 = true) {
+                itemAppeared = true
+            }
 
-            )
+            AnimatedVisibility(
+                visible = itemAppeared && !isDismissed,
+                enter = expandVertically(
+                    animationSpec = tween(
+                        durationMillis = 300
+                    )
+                ),
+                exit = shrinkVertically(
+                    animationSpec = tween(
+                        durationMillis = 300
+                    )
+                )
+            ) {
+                SwipeToDismiss(
+                    state = dismissState,
+                    directions = setOf(DismissDirection.EndToStart),
+                    dismissThresholds = {
+                        FractionalThreshold(fraction = 0.2f)
+                    },
+                    background = {
+                        RedBackground(degrees = degrees)
+                    },
+                    dismissContent = {
+                        TaskItem(
+                            toDoTask = task,
+                            navigateToTaskScreen = { navigateToTaskScreen(task.id) }
+                        )
+                    }
+
+                )
+            }
         }
     }
 }
